22ab56fac14edc40af009009ed59c3b1317cac75
sulaiiiman <85599803+sulaiiiman@users.noreply.github.com>
2021-12-26 08:19:40 +0600

    otc

---
+++ Otc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X GET https://circle.robi.com.bd/mylife/appapi/appcall.php?op=getOTC&pin=73636838&app_version=78&msisdn=8801833268701 -H "x-app-key: 000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg" 
